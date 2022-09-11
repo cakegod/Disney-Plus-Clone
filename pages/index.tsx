c724c125af61d2744b9665fce835d604aa4fcd19
Cake <cakemancerr@gmail.com>
2022-09-11 13:55:14 +0200

    Add button transition

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
 
 			<main className='h-screen '>
 				<div className='lg:bg-[url("../public/assets/pc-background.jpeg")] bg-[url("../public/assets/mobile-background.jpeg")] bg-cover bg-[position:50%] h-full absolute z-0 top-0 bottom-0 right-0 left-0 lg:px-16 px-8 py-6 flex justify-center lg:justify-start'>
-					<button className='uppercase border border-gray bg-black/90 w-20 h-12 lg:w-28 lg:h-12 text-lg lg:text-xl tracking-wide rounded-md ml-auto'>
+					<button className='uppercase border border-white hover:bg-white hover:text-black transition-all hover:transition-all bg-black/90 w-20 h-12 lg:w-28 lg:h-12 text-lg lg:text-xl tracking-wide rounded-md right-8 fixed'>
 						Log in
 					</button>
 					<div className='fixed flex flex-col gap-4 items-center max-w-lg lg:text-xl lg:items-start px-8 lg:px-0 pt-40 text-center lg:text-left'>
@@ -36,7 +36,7 @@ const Home: NextPage = () => {
 								type='email'
 								className='lg:w-80 h-14 bg-gray-800 rounded-l z-10 focus:border focus:border-gray-500 outline-0 px-2 '
 							/>
-							<button className='lg:w-56 h-14 bg-blue-600 hover:bg-blue-500 rounded-r uppercase tracking-wider text-white text-lg lg:text-xl'>
+							<button className='lg:w-56 h-14 bg-blue-600 hover:bg-blue-500 transition-all hover:transition-all rounded-r uppercase tracking-wider text-white text-lg lg:text-xl'>
 								Continue
 							</button>
 						</form>
