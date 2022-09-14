f7d3897f43862b9a1a1111ddfc42bbdad5f3185c
Cake <cakemancerr@gmail.com>
2022-09-14 15:26:01 +0200

    Add home list section

--- components/home/EmailForm.tsx
+++ components/home/EmailForm.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/future/image';
+import SignInUp from './SignUpButton';
 
 export default function EmailForm() {
 	return (
-		<div className='fixed flex flex-col gap-4 items-center max-w-lg lg:text-xl lg:items-start px-8 lg:px-0 pt-40 text-center lg:text-left'>
+		<div className=' flex flex-col gap-4 items-center max-w-lg lg:text-xl lg:items-start lg:px-0 pt-60 text-center lg:text-left'>
 			<Image src='/assets/logo.svg' width={180} height={97} alt='disney logo' />
 			<h1 className='text-white z-10 text-2xl lg:text-4xl font-medium tracking-wide'>
 				+ More than you&apos;d ever imagine
@@ -11,15 +12,13 @@ export default function EmailForm() {
 
 			<form
 				action=''
-				className='flex flex-col lg:flex-row lg:h-14 gap-2 lg:gap-0  w-full'>
+				className='flex flex-col items-center lg:flex-row lg:h-14 gap-2 lg:gap-0 w-full'>
 				<input
 					placeholder='Email'
 					type='email'
-					className='lg:w-80 h-14 bg-gray-800 rounded-l z-10 focus:border focus:border-gray-500 outline-0 px-2 '
+					className='lg:max-w-80 w-full h-14 bg-gray-800 rounded-l z-10 focus:border focus:border-gray-500 outline-0 px-2 max-w-xs'
 				/>
-				<button className='lg:w-56 h-14 bg-blue-600 hover:bg-blue-500 transition-all hover:transition-all rounded-r uppercase tracking-wider text-white text-lg lg:text-xl'>
-					Continue
-				</button>
+				<SignInUp />
 			</form>
 			<p>
 				Get 12 months for the price of 10 when you sign up for an annual Disney+
---
+++ components/home/SignUpButton.tsx
@@ -0,0 +1,7 @@
+export default function SignInUp() {
+	return (
+		<button className='lg:w-56 h-14 bg-blue-600 hover:bg-blue-500 transition-all hover:transition-all uppercase tracking-wider text-white text-lg lg:text-xl w-full rounded lg:rounded-none lg:rounded-r max-w-xs m-6'>
+			Sign in up
+		</button>
+	);
+}
